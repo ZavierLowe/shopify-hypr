ec0dfe9254bb86d204cc1636df1d0260a88d7f96
zavier <zavier.lowe@gmail.com>
2021-12-30 13:04:06 -0500

    header styling and added vue plus axios

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,55 @@
 // Put your application javascript here
+
+const store = Vue.reactive({
+
+      state:{
+            cartState:[]
+      },
+
+      getCart(){
+            axios.get('/cart.js')
+            .then(response =>{
+                  this.state.cartState.unshift(response.data)
+            })
+            .catch(error => {
+                  console.log(error);
+            })
+      }
+})
+
+const miniCartState = Vue.reactive({
+
+      hidden:true
+})
+
+const toggleMiniCart = {
+
+      //Toggle the ministate of the cart
+      openCart(){
+            miniCartState.hidden = !miniCartState.hidden
+            menuState.hidden = true
+      }
+}
+
+const menuState = Vue.reactive({
+      
+      hidden: true
+})
+
+
+const toggleMenu = {
+
+      openMenu(){
+            menuState.hidden = !menuState.hidden
+            miniCartState.hidden = true
+      }
+}
+
+const toggleClose = {
+      //Close everything when the person clicks on the screen
+      closeAll(){
+
+            miniCartState.hidden = true
+            menuState.hidden = true
+      }
+}
